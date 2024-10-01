00665d6edc420f12517feba32e73b6055a83c362
Alperen ATASEVEN <alperenataseven.3@gmail.com>
2024-10-01 17:45:17 +0300

    update again..

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
 import './style.css'
 import Navbar from './Components/Navbar'
 import Progress from './Components/Progress'
 import Home from './Components/Home'
 import About from './Components/About'
-import Talents from './Components/Talents'
-import Projects from './Components/Projects'
 import Contact from './Components/Contact'
 import Footer from './Components/Footer'
 
@@ -19,8 +16,6 @@ function App() {
       <main>
         <article id='article1'><Home /></article>
         <article id='article2'><About /></article>
-        <article id='article3'><Talents /></article>
-        <article id='article4'><Projects /></article>
         <article id='article5'><Contact /></article>
         <Footer />
       </main>
